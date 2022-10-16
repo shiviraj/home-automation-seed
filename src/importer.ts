d97831dfd741d1d4c32501c6665ea0d48bfee4ea
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-10-16 11:04:03 +0530

    Shivi | update devices and add routines

--- src/importer.ts
+++ src/importer.ts
@@ -4,20 +4,30 @@ import path from "path";
 import {DB} from "raspberrypi-db/lib/pi/db";
 
 const csvImporter = async (db: DB, fileName: string, collectionName: string) => {
-    const pathName = path.join(__dirname, `./resource/${fileName}`);
-    const csvData = fs.readFileSync(pathName, "utf-8");
-    const [header, ...listItems] = csvData.split("\n").map(row => row.split(","))
-    const items = listItems.map(itemRow => {
-        return header.reduce((item: Record<string, any>, headerItem: string, index: number) => {
-            item[headerItem] = Number.isNaN(+itemRow[index]) ? itemRow[index] : +itemRow[index];
-            return item
-        }, {})
-    })
+  const pathName = path.join(__dirname, `./resource/${fileName}`);
+  const csvData = fs.readFileSync(pathName, "utf-8");
+  const [header, ...listItems] = csvData.split("\n").map(row => row.split(","))
+  const items = listItems.map(itemRow => {
+    return header.reduce((item: Record<string, any>, headerItem: string, index: number) => {
+      item[headerItem] = Number.isNaN(+itemRow[index]) ? itemRow[index] : +itemRow[index];
+      return item
+    }, {})
+  })
 
-    const collection = db.collection(collectionName);
-    await collection.dropCollection()
-    const newCollection = db.collection(collectionName);
-    await newCollection.insertMany(items)
+  const collection = db.collection(collectionName);
+  await collection.dropCollection()
+  const newCollection = db.collection(collectionName);
+  await newCollection.insertMany(items)
 }
 
-export {csvImporter}
+const jsonImporter = async (db: DB, fileName: string, collectionName: string) => {
+  const pathName = path.join(__dirname, `./resource/${fileName}`);
+  const data = fs.readFileSync(pathName, "utf-8");
+  const jsonData: Array<Record<string, any>> = JSON.parse(data)
+  const collection = db.collection(collectionName);
+  await collection.dropCollection()
+  const newCollection = db.collection(collectionName);
+  await newCollection.insertMany(jsonData)
+}
+
+export {csvImporter, jsonImporter}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import {PiClient} from "raspberrypi-db";
-import {csvImporter} from "./importer";
+import {csvImporter, jsonImporter} from "./importer";
 
 const url: string = process.env.DB_URL!
 const databaseName: string = process.env.DB_NAME!
 
 const main = async () => {
-    const piClient = new PiClient(url);
-    await piClient.connect()
-    const db = piClient.db(databaseName);
-    await csvImporter(db, "devices.csv", "devices")
-    return "done"
+  const piClient = new PiClient(url);
+  await piClient.connect()
+  const db = piClient.db(databaseName);
+  await csvImporter(db, "devices.csv", "devices")
+  await jsonImporter(db, "routine.json", "routines")
+  await jsonImporter(db, "variables.json", "variables")
+  return "done"
 }
 
 main().then(console.log).catch(console.error)
